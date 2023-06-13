1f05961a9b7cc0641ac5b588d168ffcf9bcfcb14
Carlos Moran letran <cmoran-l@c3r2s2.42malaga.com>
2023-06-13 15:29:36 +0200

    Prompt displayed

--- Makefile
+++ Makefile
@@ -2,16 +2,23 @@ SRCS	=	minishell.c
 
 OBJS	=	${SRCS:.c=.o}
 
-GCC		=	gcc -Wall -Werror -Wextra
+GCC		=	gcc -Wall -Werror -Wextra -g
 
 RM		=	rm -rf
 
 NAME	=	minishell
 
+RL_LIB	=	-I/Users/$(USER)/.brew/opt/readline/include
+
+RL_LIB_LINK	=	-L/Users/$(USER)/.brew/opt/readline/lib -lreadline
+
+.c.o:
+		${GCC} -c $< ${RL_LIB} -o ${SRCS:.c=.o}
+
 all:	$(NAME)
 
 $(NAME):	${OBJS}
-		${GCC} ${OBJS} -o ${NAME}
+		${GCC} ${OBJS} ${RL_LIB_LINK} -o ${NAME}
 
 clean:
 		${RM} ${OBJS}
--- minishell.c
+++ minishell.c
@@ -6,23 +6,48 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:33:00 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/06/12 14:18:45 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/06/13 15:27:05 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+static void ft_leaks()
 {
-	//ignore Ctrl-\ Ctrl-C signals
-	
+	system("leaks -q minishell");
+}
+
+static void	ft_ignore_signal(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	atexit(ft_leaks);
+	char	*linebuffer;
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	//ignore Ctrl-\ signal
+	ft_ignore_signal();
 	while(1)
 	{
 		//keep getline in a loop in case interruption occurs
-		//userpressed CTRL-D to exit
+		linebuffer = readline("Conchita $>");
+		if (*linebuffer == 0)
+		{
+			free(linebuffer);
+			break;
+		}
+		add_history(linebuffer);
+		printf("%s\n", linebuffer);
+		free(linebuffer);
+		//user pressed CTRL-D to exit
 		//lexically analyze and build a list o tokens
-		///parse the tokens into an abstract syntax tree
-		///free the structures
+		//parse the tokens into an abstract syntax tree
+		//free the structures
 	}
+	rl_clear_history();
 	return (0);
 }
