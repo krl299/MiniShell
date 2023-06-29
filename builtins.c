a4d8dde3f6b003eb16784b2f39562a24f06075f5
krl299 <kr2moran99@gmail.com>
2023-06-29 14:48:51 +0200

    doing builtins identifier

--- builtins.c
+++ builtins.c
@@ -6,11 +6,69 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 18:17:40 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/06/29 12:59:21 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/06/29 14:46:46 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+
+static char	**ft_create_args(t_token *tokens)
+{
+	int	i;
+	char	**args;
+
+	i = 0;
+	while (tokens[++i].type == NO_QUOTE)
+	args = malloc(sizeof(char *) * i);
+	if (!args)
+		return (0);
+	args[i] = 0;
+	i = 0;
+	while (tokens[++i].type == NO_QUOTE)
+	{
+		args[i - 1] = tokens[i].string;
+	}
+	return (args);
+}
+
+static void	ft_delete_args(char **args)
+{
+	char *aux;
+
+	aux = *args;
+}
+void	ft_built(t_token *tokens)
+{
+	char **args;
+	if (ft_strcmp(tokens->string, "pwd") == 0)
+		ft_built_pwd();	
+	else if (ft_strcmp(tokens->string, "echo") == 0)
+	{
+		args = ft_create_args(tokens);
+		ft_built_echo(args);
+		ft_delete_args(args);
+	}
+	else if (ft_strcmp(tokens->string, "cd") == 0)
+	{
+
+	}
+	else if (ft_strcmp(tokens->string, "export") == 0)
+	{
+
+	}
+	else if (ft_strcmp(tokens->string, "unset") == 0)
+	{
+
+	}
+	else if (ft_strcmp(tokens->string, "env") == 0)
+	{
+
+	}
+	else if (ft_strcmp(tokens->string, "exit") == 0)
+	{
+
+	}
+}
 
 void	ft_built_pwd(void)
 {
