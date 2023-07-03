9c994b28b82ba19bfccf596c65e5e001354e36c9
Javier Matas <javier.matas@internaliagroup.com>
2023-07-03 18:19:44 +0200

    quotes parsing fixed

--- src/parser/parser_fixer.c
+++ src/parser/parser_fixer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_fixer.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmatas-p <jmatas-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmatas-p <jmatas-p@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 18:17:57 by jmatas-p          #+#    #+#             */
-/*   Updated: 2023/07/03 15:07:21 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/07/03 18:07:53 by jmatas-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,39 +48,20 @@ void	ft_delete_quotes(t_data *data)
 	tmp = data->tokens;
 	while (tmp)
 	{
-		if (tmp->type == DOUBLE_QUOTE || tmp->type == SINGLE_QUOTE)
+		if (tmp->type == DOUBLE_QUOTE)
 		{
-			str = ft_strtrim(tmp->string, "\"\'");
+			str = ft_strtrim(tmp->string, "\"");
+			free(tmp->string);
+			tmp->string = str;
+		}
+		else if (tmp->type == SINGLE_QUOTE)
+		{
+			str = ft_strtrim(tmp->string, "\'");
 			free(tmp->string);
 			tmp->string = str;
 		}
 		tmp = tmp->next;
 	}
-}
-
-void	ft_process_quoted_env(t_token *token)
-{
-	char	*str;
-	t_token	*new_token;
-	t_token	*next_token;
-	char	*dollar;
-
-	str = token->string;
-	new_token = NULL;
-	next_token = token->next;
-	dollar = ft_strchr(str, '$');
-	*dollar = '\0';
-	token->string = ft_strdup(str);
-	token->type = DOUBLE_QUOTE;
-	new_token = malloc(sizeof(t_token));
-	new_token->string = ft_strdup(dollar + 1);
-	new_token->type = ENVI_VAR;
-	new_token->prev = token;
-	new_token->next = next_token;
-	token->next = new_token;
-	if (next_token != NULL)
-		next_token->prev = new_token;
-	free(str);
 }
 
 void	ft_set_env_variables(t_data *data)
@@ -92,8 +73,6 @@ void	ft_set_env_variables(t_data *data)
 	{
 		if (tmp->type == NO_QUOTE && ft_strchr(&tmp->string[0], '$'))
 			tmp->type = ENVI_VAR;
-		else if (tmp->type == DOUBLE_QUOTE && ft_strchr(tmp->string, '$'))
-			ft_process_quoted_env(tmp);
 		tmp = tmp->next;
 	}
 }
