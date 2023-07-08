1490aafafe6e88946ca927cc89ccbb7f4a63f1e0
Javier Matas Palao <jmatas-p@c3r1s6.42malaga.com>
2023-07-08 18:10:52 +0200

    quotes fixed + space tokens flag added

--- includes/parser.h
+++ includes/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmatas-p <jmatas-p@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jmatas-p <jmatas-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 14:26:46 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/07/05 19:38:49 by jmatas-p         ###   ########.fr       */
+/*   Updated: 2023/07/08 18:07:12 by jmatas-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,14 @@ enum e_datatype
 	IN_RED,
 	OUT_RED,
 	APPEND_RED,
-	HERE_DOC_RED
+	HERE_DOC_RED,
+	ARGUMENT
 };
 
 typedef struct s_token
 {
 	char			*string;
+	int				has_space;
 	enum e_datatype	type;
 	struct s_token	*next;
 	struct s_token	*prev;
@@ -49,6 +51,7 @@ typedef struct s_data
 void	ft_init_data(t_data *data, int argc, char **argv, char **envp);
 
 // PARSER.C
+int		ft_check_space(t_data *data, int cur);
 void	ft_print_tokens(t_data *data);
 int		ft_is_builtin(char *str);
 void	ft_create_tokens(t_data *data);
@@ -63,7 +66,7 @@ void	ft_set_env_variables(t_data *data);
 void	ft_parse_data(t_data *data);
 
 // PARSER_ENV.C
-int	ft_replace_var(t_token *input, t_data *data);
+int		ft_replace_var(t_token *input, t_data *data);
 
 // CLEANER.C
 void	ft_clean_tokens(t_data *data);
