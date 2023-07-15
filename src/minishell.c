9895844a500a31f58979cea0ae288bb0f1bcb159
Carlos Moran letran <cmoran-l@c3r1s2.42malaga.com>
2023-07-15 19:22:28 +0200

    now detect if there is redir

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jmatas-p <jmatas-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:33:00 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/07/15 17:50:53 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/07/15 19:17:02 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,17 @@ static void	ft_command(t_data *data, int infd, int outfd)
 	int	exist_redir;
 
 	exist_redir = 0;
-	while (data->aux_tkn && data->aux_tkn->type != 7)
+	while (data->aux_tkn && data->aux_tkn->type != PIPE)
 	{
 		if (data->aux_tkn->type == IN_RED || data->aux_tkn->type == OUT_RED || data->aux_tkn->type == APPEND_RED || data->aux_tkn->type == HERE_DOC_RED)
-		{
 			exist_redir = 1;
-			break;
-		}
-		data->aux_tkn = data->aux_tkn->prev;
+		data->aux_tkn = data->aux_tkn->next;
 	}
 	if (exist_redir)//there are a redirecction before last token or pipe
 		ft_redir(data, infd, outfd);
 	else
 		ft_process_commands(data, infd, outfd);//need files because there are pipes after
 }
-		
 
 static void	ft_do_commands(t_data *data)
 {
@@ -68,7 +64,6 @@ static void	ft_do_commands(t_data *data)
 			ft_command(data, infd, pipefd[1]);
 		close(pipefd[1]);
 		infd = pipefd[0];
-		//printf("pipefd[0] = %d \t pipe[1] = %d\n", pipefd[0], pipefd[1]);
 		while (data->tokens && data->tokens->type != PIPE)
 			data->tokens = data->tokens->next;
 		if (data->tokens && data->tokens->type == PIPE)
@@ -102,7 +97,6 @@ int	main(int argc, char **argv, char **envp)
 		ft_create_tokens(&data);
 		ft_print_tokens(&data); // To be removed
 		ft_do_commands(&data);
-//		ft_process_commands(&data);
 		ft_clean_tokens(&data);
 	}
 	ft_clean_exit(EXIT_SUCCESS, &data);
