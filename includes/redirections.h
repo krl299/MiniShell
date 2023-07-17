fd89898b11cb547a0779c2364e09d19d026d3177
Carlos Moran letran <cmoran-l@c3r5s3.42malaga.com>
2023-07-17 18:15:48 +0200

    doing here_doc

--- includes/redirections.h
+++ includes/redirections.h
@@ -6,7 +6,7 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 00:36:26 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/07/17 08:39:03 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/07/17 15:41:36 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	ft_output_redir(t_data *data, int outfd);
 
 void	ft_append_redir(t_data *data, int outfd);
 
-void	ft_here_doc_redir(t_data *data, int infd);
+void	ft_here_doc_redir(t_data *data, int *infd);
 
 #endif
--- src/redirections/redirections.c
+++ src/redirections/redirections.c
@@ -6,7 +6,7 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 00:25:00 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/07/17 08:43:47 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/07/17 18:12:28 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 void	ft_redir(t_data *data, int infd, int outfd)
 {
 	if (data->aux_tkn->type == IN_RED)
-	{
 		ft_input_redir(data, &infd);
-		ft_process_commands(data, infd, outfd);
-	}
 	else if (data->aux_tkn->type == OUT_RED)
 		ft_output_redir(data, outfd);
 	else if (data->aux_tkn->type == APPEND_RED)
 		ft_append_redir(data, outfd);
 	else if (data->aux_tkn->type == HERE_DOC_RED)
-		ft_here_doc_redir(data, infd);
+		ft_here_doc_redir(data, &infd);
 }
