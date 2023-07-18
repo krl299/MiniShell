a2bf04dcf0a3f47111449f2f450851e417df64ab
krl299 <kr2moran99@gmail.com>
2023-07-18 13:33:11 +0200

    finally done here_doc

--- src/redirections/here_doc_redir.c
+++ src/redirections/here_doc_redir.c
@@ -6,22 +6,11 @@
 /*   By: cmoran-l <cmoran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 18:45:02 by cmoran-l          #+#    #+#             */
-/*   Updated: 2023/07/18 02:57:23 by cmoran-l         ###   ########.fr       */
+/*   Updated: 2023/07/18 13:31:19 by cmoran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-static char	*ft_get_pathfd(t_data *data)
-{
-	char	*shell;
-	char	*tmp;
-
-	shell = ft_get_env("SHELL", data);
-	tmp = ft_strjoin(shell, "/tmp/.here_docminishell.tmp");
-	free(shell);
-	return (tmp);
-}
 
 static char	*ft_get_delimiter(t_token *tkn)
 {
@@ -39,10 +28,8 @@ void	ft_here_doc_redir(t_data *data, int *infd)
 	int		tmpfd;
 	char	*delimiter;
 	char	*tmp_str;
-	char	*pathfd;
 
 	delimiter = ft_get_delimiter(data->aux_tkn);
-	pathfd = ft_get_pathfd(data);
 	tmpfd = open(".tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	tmp_str = get_next_line(STDIN_FILENO);
 	while (ft_strcmp(tmp_str, delimiter) != 0)
@@ -54,9 +41,9 @@ void	ft_here_doc_redir(t_data *data, int *infd)
 	close(tmpfd);
 	tmpfd = open(".tmp", O_RDONLY);
 	free(delimiter);
-	free(pathfd);
 	free(tmp_str);
 	if (*infd != STDIN_FILENO)
 		close(*infd);
 	*infd = tmpfd;
+	unlink(".tmp");
 }
